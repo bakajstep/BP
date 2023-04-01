15dde2d61493e97a7cb88d4fbce9deb02d53e59d
sbakaj <stepan.bakaj@gmail.com>
2023-04-01 11:32:57 +0200

    parent 3b06de505591786728508b5ebde3db75aa1ad573 author sbakaj <stepan.bakaj@gmail.com> 1680341577 +0200 committer sbakaj <stepan.bakaj@gmail.com> 1683053174 +0200
    parent 3b06de505591786728508b5ebde3db75aa1ad573
    author sbakaj <stepan.bakaj@gmail.com> 1680341577 +0200
    committer sbakaj <stepan.bakaj@gmail.com> 1683053043 +0200
    
    Update frontendproxy source file
    
    Update FluentBit
    
    Create first experiment and update compose file
    
    Update Dome app to 1.4 beta
    
    Update collector enviroment
    
    Update collecotr
    
    Update grafana dashboard
    
    Create ddos experiment
    
    Update grafana and collector version
    
    Send fake syslog message
    
    Final touch
    
    Update grafana and collector version
    
    Final touch

---
+++ experiments/03/03.py
@@ -0,0 +1,23 @@
+import os, requests, threading
+
+# Define a function to make the POST request
+def make_request():
+    while True:
+        response = requests.get("http://192.168.1.8:8080/api/currency")
+        print(response.text)
+
+# Create a list of threads
+threads = []
+for i in range(100):
+    # Create a new thread
+    thread = threading.Thread(target=make_request)
+    # Add the thread to the list
+    threads.append(thread)
+
+# Start the threads
+for thread in threads:
+    thread.start()
+
+# Wait for the threads to finish
+for thread in threads:
+    thread.join()
